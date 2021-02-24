4cfb7a79382160e239079f16f3fb1d5db9ee293a
XINGGou <1546602112@qq.com>
2021-02-24 15:50:59 +0800

    改了一个小bug

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,7 @@ import router from '../router'
 axios.interceptors.response.use(success => {
     //业务逻辑错误
     if (success.status && success.status == 200) {
-        if (sucess.data.code == 500 || success.data.code == 401 || success.data.code == 403) {
+        if (success.data.code == 500 || success.data.code == 401 || success.data.code == 403) {
             Message.error({message: success.data.message});
             return;
         }
